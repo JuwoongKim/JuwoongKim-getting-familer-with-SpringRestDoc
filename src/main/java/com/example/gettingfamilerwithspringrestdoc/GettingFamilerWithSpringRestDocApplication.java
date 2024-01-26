e889abe2896e375e2ea6c647247445764f308a74
juwoong kim <wwong7247@gmail.com>
2024-01-26 20:02:32 +0900

    학습용 프로젝트 초기화

---
+++ src/main/java/com/example/gettingfamilerwithspringrestdoc/GettingFamilerWithSpringRestDocApplication.java
@@ -0,0 +1,13 @@
+package com.example.gettingfamilerwithspringrestdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingFamilerWithSpringRestDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GettingFamilerWithSpringRestDocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gettingfamilerwithspringrestdoc/GettingFamilerWithSpringRestDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gettingfamilerwithspringrestdoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GettingFamilerWithSpringRestDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
